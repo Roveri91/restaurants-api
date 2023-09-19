a2dcfd4ffb47f0e27a69b48fcd8d89c457128c45
Simone Roveri <spikeroveri91@gmail.com>
2023-09-19 20:34:46 +0900

    update page works, forgot to add update route

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   before_action :set_restaurant, only: [:show, :update]
 
   def index
@@ -8,7 +9,7 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   def show
   end
 
-  def updated
+  def update
     if @restaurant.update(restaurant_params)
       render :show
     else
