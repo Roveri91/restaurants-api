c685388aab2ce303e75d38230528ec48001411a8
Simone Roveri <spikeroveri91@gmail.com>
2023-09-22 18:53:11 +0900

    create some real restaurant and fake commentss

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,35 @@ p "creating fake user"
   p "create user #{name}"
 end
 
+Restaurant.create(name: "Soggiorno", address: "5 Chome-11-38 Onoharanishi, Minoh, Osaka", category: "Pizza", rating: 4.6, phone_number:  "072-735-7757", user: User.all.sample)
+p "create Soggiorno"
 
+Restaurant.create(name: "At the 21", address: "3 Chome-18-19 Uenohigashi, Toyonaka, Osaka", category: "Okonomiyaki", rating: 4.2, phone_number:  "06-6848-3660", user: User.all.sample)
+p "create At the 21"
+
+Restaurant.create(name: "La Lanterna di Genova", address: "3 Chome-2-8 Nakazaki, Kita Ward, Osaka", category: "Italian", rating: 4.6, phone_number:  "06-6371-0606", user: User.all.sample)
+p "create La lanterna"
+
+Restaurant.create(name: "Senri Tajimaya", address: "3 Chome-10-17 Kamishinden, Toyonaka, Osaka", category: "Yakiniku", rating: 4.3, phone_number:  " 06-6833-1129", user: User.all.sample)
+p "create Senri Tajimaya"
+
+Restaurant.create(name: "Shichirin Minoh Onohara", address: "5 Chome-8-3 Onoharahigashi, Minoh, Osaka", category: "Yakiniku", rating: 4.0, phone_number:  "072-726-3558", user: User.all.sample)
+p "create Shichirin"
+
+Restaurant.create(name: "El Pancho", address: "1 Chome-10-1 心斎橋タワービル 8F", category: "Mexican", rating: 4.3, phone_number: "06-6241-0588", user: User.all.sample)
+p "El Pancho"
+
+12.times do
+  content = Faker::Restaurant.review
+  user = User.all.sample
+  restaurant = Restaurant.all.sample
+  owner = restaurant.user
+  while user == owner
+    user = User.all.sample
+  end
+  Comment.create(content: content, user: user, restaurant: restaurant)
+  p "create comment by #{user} on #{restaurant.name}"
+end
 
 
 p "~~~~DONE!~~~~~~"
